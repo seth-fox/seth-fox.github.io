93d4371b4855661196b1a60bbfa4671ebb00b122
Seth <maxheadroomincident@protonmail.com>
2022-01-22 02:45:23 -0800

    formatting/indentation

--- js/music-list.js
+++ js/music-list.js
@@ -1,42 +1,41 @@
 // https://github.com/mdbootstrap/mdb-easydata 
 
-    {
-      {
-        const API_KEY = "AIzaSyA6DLqg2QZKLEsIiX9V86wKTMU3vt_4bkI";
+{
+  {
+    const API_KEY = "AIzaSyA6DLqg2QZKLEsIiX9V86wKTMU3vt_4bkI";
 
-        function displayResult2(response) {
-          let tableHead = "";
-          let tableBody = "";
+    function displayResult2(response) {
+      let tableHead = "";
+      let tableBody = "";
 
-          response.result.values.forEach((row, index) => {
-            if (index === 0) {
-              tableHead += "<tr>";
-              row.forEach((val) => (tableHead += "<th>" + val + "</th>"));
-              tableHead += "</tr>";
-            } else {
-              tableBody += "<tr>";
-              row.forEach((val) => (tableBody += "<td>" + val + "</td>"));
-              tableBody += "</tr>";
-            }
-          });
-
-          document.getElementById("table-head").innerHTML = tableHead;
-          document.getElementById("table-body").innerHTML = tableBody;
+      response.result.values.forEach((row, index) => {
+        if (index === 0) {
+          tableHead += "<tr>";
+          row.forEach((val) => (tableHead += "<th>" + val + "</th>"));
+          tableHead += "</tr>";
+        } else {
+          tableBody += "<tr>";
+          row.forEach((val) => (tableBody += "<td>" + val + "</td>"));
+          tableBody += "</tr>";
         }
+      });
 
-        function loadData() {
-          // Spreadsheet ID
-          const spreadsheetId = "1fJmRSsipxQsATydQY9K19B2HY_iwxEb4rmM9MQ-huhY";
-          const range = "A:Z";
-          getPublicValues({ spreadsheetId, range }, displayResult2);
-        }
-
-        window.addEventListener("load", (e) => {
-          initOAuthClient({ apiKey: API_KEY });
-        });
-
-        document.addEventListener("gapi-loaded", (e) => {
-          loadData();
-        });
-      }
+      document.getElementById("table-head").innerHTML = tableHead;
+      document.getElementById("table-body").innerHTML = tableBody;
     }
+
+    function loadData() {
+      const spreadsheetId = "1fJmRSsipxQsATydQY9K19B2HY_iwxEb4rmM9MQ-huhY";
+      const range = "A:Z";
+      getPublicValues({ spreadsheetId, range }, displayResult2);
+    }
+
+    window.addEventListener("load", (e) => {
+      initOAuthClient({ apiKey: API_KEY });
+    });
+
+    document.addEventListener("gapi-loaded", (e) => {
+      loadData();
+    });
+  }
+}
